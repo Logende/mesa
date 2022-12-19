1ce6fd0c4e6c251ad2bc925e2f8da0ae8d59fc2d
Felix Neubauer <felix@neuby.de>
2022-12-19 22:01:13 +0100

    wrote unit tests for modelcachable

---
+++ tests/test_modelcachable.py
@@ -0,0 +1,105 @@
+from mesa.modelcachable import Model, ModelCachable, CacheState
+
+from unittest.mock import MagicMock
+import unittest
+from tempfile import TemporaryDirectory
+from pathlib import Path
+
+
+class ModelFibonacci(Model):
+    previous = 0
+    current = 1
+
+    def step(self):
+        new_value = self.previous + self.current
+        self.previous = self.current
+        self.current = new_value
+        if new_value > 1000:
+            self.running = False
+
+    def custom_model_function(self):
+        return self.current
+
+
+class TestModelCachable(unittest.TestCase):
+
+    def test_model_attribute_access_over_wrapper(self):
+        model = ModelFibonacci()
+        model = ModelCachable(model, "todo", CacheState.WRITE)
+        assert model.running is True
+        assert model.previous == 0
+        assert model.custom_model_function() == 1
+
+    def test_cache_read_fail_when_invalid_input(self):
+        with TemporaryDirectory() as tmp_dir_path:
+            cache_file_path = Path(tmp_dir_path).joinpath("cache_file_not_existing")
+
+            # Simulate
+            model_simulate = ModelFibonacci()
+
+            # No exception when constructing ModelCachable with CacheState.WRITE because does not try to read cache
+            ModelCachable(model_simulate, cache_file_path, CacheState.WRITE)
+
+            # Exception when trying to construct ModelCachable with CacheState.READ and non-existing cache file
+            self.assertRaises(Exception, ModelCachable, model_simulate, cache_file_path, CacheState.READ)
+
+    def test_compare_replay_with_simulation(self):
+        with TemporaryDirectory() as tmp_dir_path:
+            cache_file_path = Path(tmp_dir_path).joinpath("cache_file")
+            step_count = 100
+            values_simulate = []
+            values_replay = []
+
+            # Simulate
+            model_simulate = ModelFibonacci()
+            model_simulate = ModelCachable(model_simulate, cache_file_path, CacheState.WRITE)
+            for i in range(step_count):
+                model_simulate.step()
+                values_simulate.append(model_simulate.current)
+            model_simulate.write_cache_file()
+
+            # Replay
+            model_replay = ModelFibonacci()
+            # Mock the actual 'step' function to show that we do not simulate again, but instead the replay does work
+            mock_function = MagicMock(name='step')
+            mock_function.side_effect = Exception('Should not be called because we do not simulate but instead replay.')
+            model_replay.step = mock_function
+            model_replay = ModelCachable(model_replay, cache_file_path, CacheState.READ)
+            for i in range(step_count):
+                model_replay.step()
+                values_replay.append(model_replay.current)
+
+            # Assert that values are identical
+            assert values_replay == values_simulate
+
+    def test_cache_size(self):
+        with TemporaryDirectory() as tmp_dir_path:
+            cache_file_path = Path(tmp_dir_path).joinpath("cache_file")
+            step_count = 100
+
+            # Simulate
+            model_simulate = ModelFibonacci()
+            model_simulate = ModelCachable(model_simulate, cache_file_path, CacheState.WRITE)
+            for i in range(step_count):
+                model_simulate.step()
+            model_simulate.write_cache_file()
+
+            # Load from cache and check cache size
+            model_replay = ModelFibonacci()
+            model_replay = ModelCachable(model_replay, cache_file_path, CacheState.READ)
+            assert len(model_replay.cache) == step_count
+
+    def test_automatic_save_after_run_finished(self):
+        with TemporaryDirectory() as tmp_dir_path:
+            cache_file_path = Path(tmp_dir_path).joinpath("cache_file")
+
+            model_simulate = ModelFibonacci()
+            model_simulate = ModelCachable(model_simulate, cache_file_path, CacheState.WRITE)
+            mock_function = MagicMock(name='write_cache_file')
+            model_simulate.write_cache_file = mock_function
+
+            assert mock_function.call_count == 0
+
+            model_simulate.run_model()
+
+            assert mock_function.call_count == 1
