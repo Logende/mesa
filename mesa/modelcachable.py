3cb02e5229ab2caaf1e6fd17ed4910ccd0681acd
Felix Neubauer <felix@neuby.de>
2022-12-20 13:18:34 +0100

    refactorings and added docs

--- mesa/modelcachable.py
+++ mesa/modelcachable.py
@@ -111,6 +111,9 @@ class ModelCachable:
         self.finish_run()
 
     def finish_run(self) -> None:
+        """Tells the caching functionality that the run is finished and operations such as writing the cache
+        file can be performed. Automatically called by the 'run_model' function after the run, but needs to be
+        manually called, when calling the steps manually."""
         if self.run_finished:
             raise RuntimeError("ModelCachable: Can not finish a run that was already finished.")
 
@@ -134,9 +137,13 @@ class ModelCachable:
         self.step_count = self.step_count + 1
 
     def step_write_to_cache(self):
+        """Is performed for every step, when 'cache_state' is 'WRITE'. Serializes the current state of the model and
+        adds it to the cache (which is a list that contains the state for each performed step)."""
         self.cache.append(self.serialize_state())
 
     def step_read_from_cache(self):
+        """Is performed for every step, when 'cache_state' is 'READ'. Reads the next state from the cache, deserializes
+        it and then updates the model state to this new state."""
         serialized_state = self.cache[self.step_count]
         self.deserialize_state(serialized_state)
 
@@ -146,21 +153,31 @@ class ModelCachable:
 
 
 class ModelCachableOptimized(ModelCachable):
+    """Extends ModelCachable by the optional functionality of compressing the state data of each individual step and
+    of decreasing the replay precision by only caching every nth step. This makes it possible to dramatically decrease
+    cache size and increase replay speed for large models."""
     def __init__(self, model: Model, cache_file_path: Union[str, Path], cache_state: CacheState,
                  precision: int = 1, compress_each_step: bool = True):
         super().__init__(model, cache_file_path, cache_state)
         self.precision = precision
         self.compress_each_step = compress_each_step
 
-    def cache_step(self):
+    def step_write_to_cache(self):
+        """Is performed for every step, when 'cache_state' is 'WRITE'. Serializes the current state of the model and
+        adds it to the cache (which is a list that contains the state for each performed step).
+        If precision is >1, only every precision-th step is stored in the cache."""
         # Cache only every nth step
         if self.step_count % self.precision == 0:
-            super().cache_step()
+            super().step_write_to_cache()
 
     def compress(self, data: bytes):
+        """Compress function that is used if the setting 'compress_each_step' is True. Can be overwritten to use
+        a different compression algorithm."""
         return gzip.compress(data)
 
     def decompress(self, data: bytes):
+        """Decompress function that is used if the setting 'compress_each_step' is True. Can be overwritten to use
+        a different decompression algorithm."""
         return gzip.decompress(data)
 
     def serialize_state(self) -> Any:
@@ -184,6 +201,8 @@ class ModelCachableOptimized(ModelCachable):
 
 
 class ModelCachableStreaming(ModelCachableOptimized):
+    """Decorator for ModelCachableOptimized, that uses buffered streams for reading and writing the cache, instead
+    of keeping the complete cache in memory."""
 
     def __init__(self, model: Model, cache_file_path: Union[str, Path], cache_state: CacheState,
                  precision: int = 1, compress_each_step: bool = True):
@@ -200,15 +219,22 @@ class ModelCachableStreaming(ModelCachableOptimized):
             self.cache_file_stream = io.open(cache_file_path, 'rb')
 
     def finish_run(self) -> None:
+        """Tells the caching functionality that the run is finished and operations such as writing the cache
+        file can be performed. Automatically called by the 'run_model' function after the run, but needs to be
+        manually called, when calling the steps manually."""
         super().finish_run()
         self.cache_file_stream.close()
 
     def step_write_to_cache(self):
+        """Is performed for every step, when 'cache_state' is 'WRITE'. Serializes the current state of the model and
+        writes it to the cache file stream."""
         serialized_state: bytes = self.serialize_state()
         _stream_write_next_chunk_size(self.cache_file_stream, len(serialized_state))
         self.cache_file_stream.write(serialized_state)
 
     def step_read_from_cache(self):
+        """Is performed for every step, when 'cache_state' is 'READ'. Reads the next state from the cache file stream,
+        deserializes it and then updates the model state to this new state."""
         chunk_length = _stream_read_next_chunk_size(self.cache_file_stream)
         if chunk_length == 0:
             print("ModelCachableLarge: reached end of cache file stream.")
@@ -218,10 +244,15 @@ class ModelCachableStreaming(ModelCachableOptimized):
             self.deserialize_state(serialized_state)
 
     def _write_cache_file(self):
-        if self._cache_state is CacheState.WRITE:
-            # end cache file with a chunk size of 0, to make EOF detectable
-            _stream_write_next_chunk_size(self.cache_file_stream, 0)
+        """Overwrites the '_write_cache_file' function of the ModelCachable class. As the file content is written
+        to the stream during each step, this function does not have to write the complete cache file.
+        It only adds an EOF hint to the cache file stream. After that, the stream can be closed and the cache file is
+        completed.
+        """
+        # end cache file with a chunk size of 0, to make EOF detectable
+        _stream_write_next_chunk_size(self.cache_file_stream, 0)
 
     def read_cache_file(self):
-        # nothing to do in advance as a stream is used to read on the go
+        """Overwrites the '_read_cache_file' function of the ModelCachable class. As the file content is read from
+        the stream during each step, this function does not have to do anything in advance."""
         return
